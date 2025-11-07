68618439d928a59db640e89db60b259be6ace87f
lumluv <arc.clone.01@gmail.com>
2025-11-08 00:27:11 +0700

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { appConfig } from './app/app.config';
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, appConfig)
+  .catch(err => console.error(err));
